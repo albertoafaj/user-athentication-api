a58f34414ab4813890e0b3b3f52bbc21dc5cdf6b
Alberto Araújo <albertoafaj@hotmail.com>
2022-08-04 09:41:15 -0300

    Criou um servidor com Express

--- environmentConfig.md
+++ environmentConfig.md
@@ -4,4 +4,11 @@ npm init
 npm install -g typescript
 tsc --init
 
+* Lib que conver o ts para o JS
 
+npm install --save-dev typescript
+npm install --save-dev @types/node
+
+* Lib para gerenciar rotas http  
+$ npm install --save express
+npm install --save @types/express
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res
+        .status(200)
+        .send({
+            json: 'json'
+        })
+})
+
+// Para subir o servidor
+
+app.listen(3000, () => {
+    console.log('Aplicação excultando na porta 3000')
+})
